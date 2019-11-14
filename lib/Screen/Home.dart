78749ec78a15b94dcfc2a87207dba003a4b86eb1
Harikrishna Raveesh <harikrishnashivam@gmail.com>
2019-11-14 11:34:48 +0530

    bottomnavigavtion added

--- lib/Screen/Home.dart
+++ lib/Screen/Home.dart
@@ -10,15 +10,21 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: Colors.blue,
-        middle: Text('Home'),
-        leading: GestureDetector(
-          child: Icon(Icons.navigate_before),
-          onTap: () {},
-        ),
-      ),
-      child: Text("hello world"),
-    );
+        navigationBar: CupertinoNavigationBar(
+            backgroundColor: Colors.blue,
+            middle: Text('Home'),
+            leading: Icon(Icons.navigate_before),
+            trailing: Icon(Icons.search)),
+        child: CupertinoTabScaffold(
+          tabBar: CupertinoTabBar(items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.call), title: Text('Call')),
+            BottomNavigationBarItem(icon: Icon(Icons.message)),
+          ]),
+          tabBuilder: (context, int i) {
+            return Center(
+                child: i == 0 ? Icon(Icons.phone) : Icon(Icons.message));
+          },
+        ));
   }
 }
